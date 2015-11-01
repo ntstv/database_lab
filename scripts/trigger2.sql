4b2e22917d2e31c125a1cd4ce4ef45eaea598d9a
Roman Shamyan <shamyan.roman@gmail.com>
2015-11-01 21:39:16 +0300

    trigger 2

---
+++ scripts/trigger2.sql
@@ -0,0 +1,36 @@
+﻿/**
+* Ограничить количество неполаченных накладных для одного клиента двумя
+*/
+
+BEGIN;
+
+CREATE OR REPLACE FUNCTION check_payed_invoice_func()
+  RETURNS trigger AS $$
+DECLARE
+  count INTEGER;
+BEGIN
+  EXECUTE '
+    SELECT count(*) FROM
+    (
+      SELECT DISTINCT invoice.*
+      FROM invoice
+      LEFT OUTER JOIN payment_order
+      ON invoice.id = payment_order.invoice_id
+      WHERE invoice.buyer_id = $1 AND
+        payment_order.invoice_id is NULL
+    ) tmp'
+   USING NEW.buyer_id INTO count;
+   IF (count >= 2) THEN
+     RAISE EXCEPTION 'buyer has more then 2 unpayend invoices';
+   ELSE
+     RETURN NEW;
+   END IF;  
+END;
+$$ LANGUAGE plpgsql;
+
+
+--CREATE TRIGGER check_payed_invoice BEFORE UPDATE OR INSERT ON "invoice"
+--  FOR EACH ROW
+--  EXECUTE PROCEDURE check_payed_invoice_func();
+
+COMMIT;
---
+++ scripts/unpayed_count.sql
@@ -0,0 +1,9 @@
+﻿SELECT count(*) FROM
+(
+  SELECT DISTINCT invoice.*
+  FROM invoice
+  LEFT OUTER JOIN payment_order
+  ON invoice.id = payment_order.invoice_id
+  WHERE invoice.buyer_id = 1 AND
+    payment_order.invoice_id is NULL
+) tmp
